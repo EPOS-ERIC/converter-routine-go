50b713ff8cf78e7f61687e5407c8ab059839e391
Marco Salvi <marco.salvi@ingv.it>
2024-12-20 12:49:56 +0100

    update db connection

--- connection/Connection.go
+++ connection/Connection.go
@@ -11,12 +11,48 @@ import (
 	"strings"
 )
 
+var hosts []string
+var params = ""
+
 // get a db to execute queries
 func Connect() (*gorm.DB, error) {
+	var err error
+	if hosts == nil && params == "" {
+		hosts, params, err = initializeHosts()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Attempt to connect to each host
+	for _, host := range hosts {
+		currentDSN := fmt.Sprintf("postgresql://%s/%s", host, params)
+		// log.Printf("Attempting to connect to: %s", host)
+		db, err := gorm.Open(postgres.New(postgres.Config{
+			DriverName: "pgx",
+			DSN:        currentDSN,
+		}), &gorm.Config{
+			NamingStrategy: schema.NamingStrategy{
+				TablePrefix:   "",   // table name prefix
+				SingularTable: true, // use singular table names
+			},
+		})
+		if err != nil {
+			// log.Printf("Failed to connect to: %s, error: %v", host, err)
+			// try next host
+			continue
+		}
+		// log.Printf("Successfully connected to: %s", host)
+		return db, nil
+	}
+	return nil, fmt.Errorf("error enstablishing connection to db: all hosts are unreachable: %w", err)
+}
+
+func initializeHosts() ([]string, string, error) {
 	dsn, ok := os.LookupEnv("POSTGRESQL_CONNECTION_STRING")
 	log.Println("POSTGRESQL_CONNECTION_STRING:", dsn)
 	if !ok {
-		return nil, fmt.Errorf("POSTGRESQL_CONNECTION_STRING is not set")
+		return nil, "", fmt.Errorf("POSTGRESQL_CONNECTION_STRING is not set")
 	}
 
 	// Remove the "jdbc:" prefix if it exists
@@ -38,14 +74,14 @@ func Connect() (*gorm.DB, error) {
 	// Parse hosts and connection parameters correctly
 	hostStart := strings.Index(dsn, "//")
 	if hostStart == -1 {
-		return nil, fmt.Errorf("invalid connection string format: missing '//'")
+		return nil, "", fmt.Errorf("invalid connection string format: missing '//'")
 	}
 
 	// Extract everything after `//` (hosts and parameters)
 	hostsAndParams := dsn[hostStart+2:]
 	splitIndex := strings.Index(hostsAndParams, "/")
 	if splitIndex == -1 {
-		return nil, fmt.Errorf("invalid connection string format: missing '/' after hosts")
+		return nil, "", fmt.Errorf("invalid connection string format: missing '/' after hosts")
 	}
 
 	hosts := hostsAndParams[:splitIndex]
@@ -56,34 +92,6 @@ func Connect() (*gorm.DB, error) {
 	log.Printf("Parsed Hosts: %v", hostList)
 	log.Printf("Connection Parameters: %s", params)
 
-	var db *gorm.DB
-	var err error
-	var lastError error
-
-	// Attempt to connect to each host
-	for _, host := range hostList {
-		currentDSN := fmt.Sprintf("postgresql://%s/%s", host, params)
-		log.Printf("Attempting to connect to: %s", host)
-		db, err = gorm.Open(postgres.New(postgres.Config{
-			DriverName: "pgx",
-			DSN:        currentDSN,
-		}), &gorm.Config{
-			NamingStrategy: schema.NamingStrategy{
-				TablePrefix:   "",   // table name prefix
-				SingularTable: true, // use singular table names
-			},
-		})
-		if err == nil {
-			log.Printf("Successfully connected to: %s", host)
-			return db, nil
-		}
-		log.Printf("Failed to connect to: %s, error: %v", host, err)
-		lastError = err // Keep track of the last error
-	}
-
-	// Return the last error if no connection is successful
-	if lastError != nil {
-		log.Printf("Returning the last error encountered: %v", lastError)
-	}
-	return nil, fmt.Errorf("all hosts are unreachable: %w", lastError)
+	return hostList, params, nil
 }
+
